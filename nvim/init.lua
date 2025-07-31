e852c966ab1c046bc87eb68cc8c7b11324205d74
xsoder <tadihailukebe@gmail.com>
2025-07-31 13:16:39 -0400

    Fixed all Stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,7 @@ vim.keymap.set('n', '<leader>g', ':Git<CR>')
 -- Plugins
 require('mini.pick').setup {}
 vim.keymap.set('n', '<leader>pf', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>ps', ':Pick grep<CR>')
 vim.keymap.set('n', '<leader>bb', ':Pick buffers<CR>')
 vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
 
@@ -45,3 +46,53 @@ vim.api.nvim_set_keymap('n', '<leader>fq', '<cmd>lua vim.lsp.buf.code_action()<C
 
 require('typst-preview').setup {}
 vim.keymap.set('n', '<leader>mp', ':TypstPreview<CR>')
+
+
+-- For testing Purposes
+ vim.filetype.add({
+ 	extension = {
+ 		code = "code",
+ 	},
+ })
+
+-- File type detection for vlogcc
+vim.api.nvim_create_autocmd('BufNewFile', {
+    pattern = { '*.code' },
+    callback = function()
+        vim.bo.filetype = 'vlogcc'
+    end
+})
+
+vim.api.nvim_create_autocmd('BufRead', {
+    pattern = { '*.code' },
+    callback = function()
+        vim.bo.filetype = 'vlogcc'
+    end
+})
+
+-- Setup custom vlogcc LSP server using raw LSP API
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('VlogccLsp', {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.name == 'vlogcc' then
+            -- Set up keymaps for vlogcc LSP
+            local opts = { buffer = args.buf }
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        end
+    end
+})
+
+-- Start the vlogcc LSP server
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'vlogc', 'code' },
+    callback = function()
+        vim.lsp.start({
+            name = 'vlogc',
+            cmd = { "/home/xsoder/programming/vlogc-lsp/main" },
+            root_dir = vim.fs.dirname(vim.fs.find({ '*.code', 'go.mod' }, { upward = true })[1] or vim.fn.getcwd()),
+        })
+    end
+})
