00fe66c3c7f554137359302ba255c54e164044f2
Mr-Emacs <tadihailukebe@gmail.com>
2026-05-11 09:24:14 -0400

    Stuff is removed

--- nvim/lua/config/lazy/color.lua
+++ nvim/lua/config/lazy/color.lua
@@ -2,5 +2,7 @@ return {
     "vague-theme/vague.nvim",
     config = function()
         vim.cmd.colorscheme('vague')
+        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
     end
 }
